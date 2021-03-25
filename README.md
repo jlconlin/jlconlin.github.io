d03dfd233afb728c449af1e9e054aae148bb97ec
jlconlin <jlconlin@users.noreply.github.com>
2021-03-25 12:36:27 +0000

    jekyll build from Action 118af5d3f0ab50f27ec52729d36c7124c337b103

---
+++ README.md
@@ -0,0 +1,5 @@
+# jlconlin.github.io
+This is my personal space. I've got a few projects that I've created for my
+personal use. 
+
+I also am involved in the [NJOY21](https://github.com/njoy21) project. 
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"I wasn’t intending to start a blog, yet here I am. For years I have kept a personal journal where I write about the days events for me and my family. I’ve also kept professional Lab Notebook—essentially a blog of my professional work as a scientist. This blog comes out...","categories": [],
+        "tags": ["Raspberry Pi","Arduino","blog","journal","Lab Notebook","minimal-mistakes","Jekyll"],
+        "url": "/blog/2021/02/25/February25",
+        "teaser": null
+      },{
+        "title": "VNC Authentication with Raspberry Pi",
+        "excerpt":"I like to use the Screen Sharing app on my Mac to connect to my Raspberry Pi so I don’t have to connect a keyboard/mouse/monitor. After it is setup, you can do this. A little more configuration needs to be done in order to make this work seamlessly with Screen...","categories": [],
+        "tags": ["VNC","Authentication"],
+        "url": "/blog/2021/03/02/VNC-Authentication-with-Raspberry-Pi",
+        "teaser": null
+      },{
+        "title": "Configuring Network Interfaces",
+        "excerpt":"In trying to get pi-hole working at home, I need to have multiple IP addresses for my Raspberry Pi server. I need this because my router requires three different IP addresses. You can create a virtual network interface temporarily by executing the command: ifconfig eth0:0 &lt;new IP address&gt; If you...","categories": [],
+        "tags": ["network","virtual interface","config"],
+        "url": "/blog/2021/03/10/Configuring-Network-Interfaces",
+        "teaser": null
+      },{
+        "title": "Working with OLED Displays",
+        "excerpt":"I’ve been working with a Raspberry Pi Zero. I want to set it up to automatically take pictures and will (eventually) set it up where it won’t be connected to a keyboard/mouse/monitor. I will still need to know the IP address so I can use VNC to connect to from...","categories": [],
+        "tags": ["OLED","Display","Adafruit","Python"],
+        "url": "/blog/2021/03/25/Working-with-OLED-Displays",
+        "teaser": null
+      }]
